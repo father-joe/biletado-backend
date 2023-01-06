b54baa407cc49de165d0a8be60738c8e2a3e53ca
Johannes V <vater.jo@gmail.com>
2023-01-06 15:14:16 +0100

    store created reservation in list

--- src/main/java/edu/dhbw_ka/webeng/biletado_backend/rest/ReservationsController.java
+++ src/main/java/edu/dhbw_ka/webeng/biletado_backend/rest/ReservationsController.java
@@ -7,28 +7,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @RestController
 public class ReservationsController {
 
+    List<Reservation> reservations = new ArrayList<>();
+
     @GetMapping(path = "/reservations")
     List<Reservation> getReservations() {
-
-        Reservation r1 = new Reservation();
-        r1.setId("id: 1");
-        r1.setFrom(new Date());
-        r1.setTo(new Date());
-        r1.setRoom_id("room_id: 1");
-
-        List<Reservation> reservations = new ArrayList<>();
-        reservations.add(r1);
         return reservations;
     }
 
     @PostMapping(path = "/reservations")
     Reservation createReservation(@RequestBody Reservation reservation) {
+        reservations.add(reservation);
         return reservation;
     }
 }
