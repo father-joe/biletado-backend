7276463cd3e89b3e5bfc25350ddf1e85484e5c91
Johannes V <vater.jo@gmail.com>
2023-01-13 16:54:21 +0100

    cosmetics

--- src/test/java/edu/dhbw_ka/webeng/biletado_backend/rest/ReservationsControllerTests.java
+++ src/test/java/edu/dhbw_ka/webeng/biletado_backend/rest/ReservationsControllerTests.java
@@ -1,27 +1,27 @@
 package edu.dhbw_ka.webeng.biletado_backend.rest;
 
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.Matchers.*;
+import edu.dhbw_ka.webeng.biletado_backend.model.Room;
+import edu.dhbw_ka.webeng.biletado_backend.service.RoomService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import java.text.*;
-
-import org.junit.jupiter.api.*;
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.boot.test.autoconfigure.web.servlet.*;
-import org.springframework.boot.test.context.*;
-import org.springframework.boot.test.mock.mockito.*;
-import org.springframework.http.*;
-import org.springframework.security.test.context.support.*;
-import org.springframework.test.context.*;
-import org.springframework.test.web.servlet.*;
-
-import edu.dhbw_ka.webeng.biletado_backend.model.*;
-import edu.dhbw_ka.webeng.biletado_backend.service.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -36,14 +36,14 @@ class ReservationsControllerTests {
     
     @Test
     void getReservations_shouldListAllWhenNoFilter() throws Exception {
-        mockMvc.perform(get("/reservations/")).andDo(print()).andExpect(status().isOk())
+        mockMvc.perform(get("/reservations/")).andDo(print())
+                .andExpect(status().isOk())
                 // expect array of size 2 from data.sql
                 .andExpect(jsonPath("$.length()", is(2)));
     }
 
     @Test
     void getReservations_shouldFilterByRoomId() throws Exception {
-
         mockMvc.perform(get("/reservations/?room_id=00000000-0000-0000-0000-000000000001")).andDo(print())
                 .andExpect(status().isOk())
                 // expect single from data.sql
